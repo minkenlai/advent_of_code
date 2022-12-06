3b20c5624f412df29d338966b7d2de03db4a68f2
Min-Ken Lai <mlai@easypost.com>
2022-12-05 23:14:11 -0800

    day6

--- aoc2022/lib.py
+++ aoc2022/lib.py
@@ -13,7 +13,7 @@ def convert(value: str, mapping: dict[str, T.Any]):
     return mapping[value]
 
 
-def all_lines():
+def all_lines(source=sys.stdin):
     return [v.strip() for v in sys.stdin]
 
 
@@ -28,7 +28,7 @@ def grouped(source, group_size=3):
             group = []
 
 
-def transpose(lines: list[str], cols: int = 0) -> list[deque]:
+def transpose(lines: T.Union[list[str], list[list]], cols: int = 0) -> list[deque]:
     cols = cols if cols else len(lines[0])
     new_lines = [deque() for i in range(0, cols)]
     for line in lines:
@@ -37,7 +37,9 @@ def transpose(lines: list[str], cols: int = 0) -> list[deque]:
     return new_lines
 
 
-def rotate(lines: list[str], cols: int = 0, clockwise: bool = False) -> list[deque]:
+def rotate(
+    lines: T.Union[list[str], list[list]], cols: int = 0, clockwise: bool = False
+) -> list[deque]:
     cols = cols if cols else len(lines[0])
     new_lines = [deque() for i in range(0, cols)]
     for line in lines:
@@ -49,6 +51,14 @@ def rotate(lines: list[str], cols: int = 0, clockwise: bool = False) -> list[deq
     return new_lines
 
 
+def has_repeat(buffer):
+    s = set()
+    for c in buffer:
+        s.add(c)
+    print(f"{s=} {buffer=}")
+    return len(s) < len(buffer)
+
+
 if __name__ == "__main__":
     assert ints("1 23 45") == [1, 23, 45]
     print(ints("1 23 45 6  78"))
@@ -57,18 +67,33 @@ if __name__ == "__main__":
         print(line)
 
     print("transpose")
-    new_lines = transpose(lines)
-    for line in new_lines:
-        print(line)
-
-    print("clockwise")
-    new_lines = rotate(lines, clockwise=True)
-    for line in new_lines:
+    transposed_lines = transpose(lines)
+    for line in transposed_lines:
         print(line)
 
     print("anti-clockwise")
     new_lines = rotate(lines, clockwise=False)
-    for line in new_lines:
+    for i, line in enumerate(new_lines):
         print(line)
+        # same as transposed then lines reversed
+        assert line == transposed_lines[len(new_lines) - 1 - i]
+
+    print("clockwise")
+    new_lines = rotate(lines, clockwise=True)
+    for i, line in enumerate(new_lines):
+        print(line)
+        # same as transposed then lines reversed-within
+        line.reverse()
+        assert line == transposed_lines[i]
+
+    # try with number lists
+    num_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    new_lists = transpose(num_lists)
+    for i, line in enumerate(new_lists):
+        print(line)
+    new_lists = rotate(num_lists, clockwise=True)
+    for i, line in enumerate(new_lists):
+        print(line)
+
 
 print(f"done {__name__}")
