ae869a5cc3a37a24dcdd2d4b98ce6332262604eb
Min-Ken Lai <mlai@easypost.com>
2022-12-08 22:08:13 -0800

    day9 p2

--- aoc2022/day9.py
+++ aoc2022/day9.py
@@ -10,29 +10,41 @@ def sample():
         "35390",
     ]
 
-def follow(head, tail) -> tuple[int, int]:
-    x, y = tail
+def follow(head, tail) -> list[tuple[int, int]]:
+    #print(f"{tail=}")
+    x, y = tail[0]
     w, z = head
-    if abs(y - z) + abs(x - w) > 2:
+    if y==z and abs(x - w) > 1:
+        if x < w:
+            x = w-1
+        elif x > w:
+            x = w+1
+    elif x==w and abs(y - z) > 1:
+        if y < z:
+            y = z-1
+        elif y > z:
+            y = z+1
+    elif abs(y - z) + abs(x - w) > 2:
         print(f" . {x=} {y=} {w=} {z=}")
         if x < w:
-            x += 1
+            x = w-1 if w - x > 1 else w
         elif x > w:
-            x -= 1
+            x = w+1 if x - w > 1 else w
         if y < z:
-            y += 1
+            y = z-1 if z - y > 1 else z
         elif y > z:
-            y -= 1
+            y = z+1 if y - z > 1 else z
         print(f" .. {x=} {y=} {w=} {z=}")
-    elif abs(x - w) > 1:
-        x = int((x + w)/2)
-    elif abs(y - z) > 1:
-        y = int((y + z)/2)
-    return (x, y)
+
+    new_tail = [(x, y)]
+    if len(tail) == 1:
+        return new_tail
+    new_tail.extend(follow((x, y), tail[1:]))
+    return new_tail
 
 _total = 0
 
-def move(head: tuple[int, int], tail: tuple[int, int], d:str, c:int, visited: set):
+def move(head: tuple[int, int], tail: list[tuple[int, int]], d:str, c:int, visited: set):
     global _total
     for i in range(c):
         ph, pt = head, tail
@@ -46,18 +58,19 @@ def move(head: tuple[int, int], tail: tuple[int, int], d:str, c:int, visited: se
         elif d=="R":
             head = (head[0], head[1]+1)
         tail = follow(head, tail)
-        print(f" {ph=} {pt=} -> {head=} {tail=}")
-        visited.add(tail)
+        visited.add(tail[-1])
     return head, tail
 
 def run(lines):
     visited=set()
     head: tuple[int, int] = (0, 0)
-    tail: tuple[int, int] = (0, 0)
+    tail: list[tuple[int, int]] = [(0, 0) for _ in range(9)]
     for l in lines:
         d, c = l.split(" ")
         c = int(c)
+        ph, pt = head, tail
         head, tail = move(head, tail, d, c, visited)
+        print(f" {d=} {c=} : {ph=} {pt=} -> {head=} {tail=}")
     print(f"===== final {_total=} {len(visited)=}")
 
 
